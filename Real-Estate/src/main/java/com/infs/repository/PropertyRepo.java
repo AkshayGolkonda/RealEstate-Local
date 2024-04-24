fbdde5fd1eb44bf2c09676316e676275c62b8048
Akshay Golkonda <85550341+AkshayGolkonda@users.noreply.github.com>
2024-04-24 00:38:12 -0400

    Delete Real-Estate directory

--- Real-Estate/src/main/java/com/infs/repository/PropertyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.infs.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.infs.document.PropertyDocument;
-
-public interface PropertyRepo extends MongoRepository<PropertyDocument, Integer>{
-
-}
